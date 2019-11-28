29cfee254b22f0cac0c16039d78c03ad605daa1e
Mansour <mndiayepro97@gmail.com>
2019-11-28 12:45:56 +0100

    test

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ if(isset($_POST['login'])){
             <option value="H">Masculin</option>
             <option value="F">Féminin</option>
     </select>
-        <p> Hello it's Mansour !</p>
+        <p> Hello it's Hugo !</p>
         <p>Votre nom : <input type="text" name="nom" placeholder="Votre nom" /></p>
         <p>Votre prénom : <input type="text" name="prenom" placeholder="Votre prénom" /></p>
         <p>Votre âge : <input type="text" name="age" placeholder="Votre âge" /></p>
